e05882fa8d491ad52a1efb3e870031b720194aa2
BrainSop <brainsop@gmail.com>
2020-04-16 13:10:57 -0300

    Update instalark8s.sh

--- instalark8s.sh
+++ instalark8s.sh
@@ -17,8 +17,6 @@ if [ $# -lt 2 ]; then
   echo "    rol = master o worker"
   echo "    nombre = nombre de host a usar"
   echo
-  echo " rol "$rol
-  echo " nombre "$nombre
   echo "==============================="
   exit
 fi
@@ -29,91 +27,34 @@ echo " Nombre: "$nombre
 echo "==============================="
 
 sudo hostnamectl set-hostname $nombre
-
-read -rsp $'Press enter to continue...\n'
-
 sudo apt update
-
-read -rsp $'Press enter to continue...\n'
-
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-
-read -rsp $'Press enter to continue...\n'
-
+sudo apt --assume-yes install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
-
-read -rsp $'Press enter to continue...\n'
-
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-
-read -rsp $'Press enter to continue...\n'
-
 sudo apt update
-
-read -rsp $'Press enter to continue...\n'
-
 apt-cache policy docker-ce
-
-read -rsp $'Press enter to continue...\n'
-
-sudo apt install docker-ce
-
-read -rsp $'Press enter to continue...\n'
-
-sudo service docker status
-
-read -rsp $'Press enter to continue...\n'
-
+sudo apt --assume-yes install docker-ce
+#el sigiente comando es solo para ver el estado
+#sudo service docker status
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-
-read -rsp $'Press enter to continue...\n'
-
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-
-read -rsp $'Press enter to continue...\n'
-
 sudo swapoff -a
-
-read -rsp $'Press enter to continue...\n'
-
-sudo apt-get install kubeadm -y
-
-read -rsp $'Press enter to continue...\n'
-
+sudo apt-get --assume-yes install kubeadm -y
 kubeadm version
 
-read -rsp $'Press enter to continue...\n'
-
 # SOLO MASTER
-kubeadm init —pod-network-cidr=10.244.0.0/16
-#guardar output para worker nodes
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 
+#guardar output para worker nodes
 read -rsp $'Guardar SALIDA...\n'
-read -rsp $'Press enter to continue...\n'
 
 mkdir -p $HOME/.kube
-
-read -rsp $'Press enter to continue...\n'
-
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-
-read -rsp $'Press enter to continue...\n'
-
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-read -rsp $'Press enter to continue...\n'
-
 sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
-read -rsp $'Press enter to continue...\n'
-
 kubectl get pods --all-namespaces
-
-read -rsp $'Press enter to continue...\n'
-
 kubectl get nodes
 
-read -rsp $'Press enter to continue...\n'
 
 # SOLO NODES
  # COMANDO DE SALIDA DEL MASTER!!!!
